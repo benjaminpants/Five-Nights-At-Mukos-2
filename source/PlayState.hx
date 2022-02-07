5410cee4cb04182ef9824dee8fac778786850523
benjaminpants <maltbyfamily@gmail.com>
2022-02-06 21:24:38 -0800

    place the holder

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,22 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var OfficeSprite:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
 
-		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
-		text.screenCenter();
-		add(text); 
+		// var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		// text.screenCenter();
+		OfficeSprite = new FlxSprite();
+		OfficeSprite.loadGraphic("assets/images/main/office.png");
+		OfficeSprite.screenCenter();
+		add(OfficeSprite);
 	}
 
 	override public function update(elapsed:Float)
